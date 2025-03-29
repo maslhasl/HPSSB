9df6bf5bbb8975b38d29fb248dfb241683765f01
master <testtset13@yandex.ru>
2025-03-30 00:23:33 +0300

    добавлено новое задание

--- 3of28tasks.py
+++ 3of28tasks.py
@@ -34,29 +34,37 @@ class DynArray:
 #1. Добавьте метод insert(i, itm), который вставляет в i-ю позицию объект itm, сдвигая вперёд все последующие элементы.
 # Учтите, что новая длина массива может превысить размер буфера.
     def insert(self, i, itm):
-        #Случай когда i < 0 и i > count
+        #Случай когда i < 0 или i > count
         if i < 0 or i > self.count:
             raise IndexError('Index is out of bounds')
-        #Случай когда count == capasity
-            #Увеличиваем capacity
-            #Случай когда i == count
-                #Добавляем itm в хвост массива
-            #Случай когда i < count
-                #Сдвигаем элементы с индексом >= i+1 вправо
-                #Добавляем itm на освободившееся место
-            #Увеличиваем count на 1
+        #Случай когда count == capacity
+        if self.count == self.capacity:
+            self.resize(2 * self.capacity) #Увеличиваем capacity
+        #Случай когда i < count
+        if i < self.count:
+            for j in range(self.count,i,-1): #Сдвигаем элементы с индексом >= i+1 вправо
+                self.array[j] = self.array[j-1]
+        #Вставляем itm на место с индексом i
+        self.array[i] = itm
+        self.count += 1 #Увеличиваем count на 1
 
-        #Случай когда count < capacity
-            #Случай когда i > count
-                #Добавляем itm в хвост массива
-            #Случай когда i < count
-                #Сдвигаем элементы с индексом >= i+1 вправо
-                #Добавляем itm на освободившееся место
-            #Увеличиваем count на 1
-    #добавляем объект itm в позицию i, начиная с 0
 
 #2. Добавьте метод delete(i), который удаляет объект из i-й позиции, при необходимости сжимая буфер.
     def delete(self, i):
+        if self.count == 0:
+            raise IndexError('DynArr is empty')
+        #Случай когда i < 0 или i > count
+        if i < 0 or i >= self.count:
+            raise IndexError('Index is out of bounds')
+        for j in range(i,self.count-1):
+            self.array[j] = self.array[j+1]
+
+        self.count -= 1 #Уменьшаем количество элементов на один
+        self.array[self.count] = None #Очистка последнего элемента, после сдвига
+
+        #Уменьшение capacity массива
+        if self.count < self.capacity//2:
+            self.resize(self.count)
         # удаляем объект в позиции i
 
 #В обоих случаях, если индекс i лежит вне допустимых границ, генерируйте исключение.
